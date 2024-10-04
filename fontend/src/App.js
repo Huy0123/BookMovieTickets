a18d527373f1e2156e1ba4c2dfe47b19099d2d25
Trung Du <nguyentrinhtrungdu@gmail.com>
2024-10-04 12:18:37 +0700

    header

--- fontend/src/App.js
+++ fontend/src/App.js
@@ -2,6 +2,7 @@ import { Fragment } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { publicRoutes } from '~/routes';
 import { DefaultLayout } from '~/Components/Layout';
+
 function App() {
   return (
     <Router>
--- fontend/src/pages/Home/index.js
+++ fontend/src/pages/Home/index.js
@@ -1,17 +1,19 @@
 import classNames from 'classnames/bind'; // Ensure this path is correct
 import styles from './Home.module.scss';
+
 const cx = classNames.bind(styles);
 
 function Home() {
     return (  
         <div className={cx('container')}>
-        <img
+
+        {/* <img
                     className={cx('img-banner')}
                     src='https://www.sterlingku.com/img/starbucks-logo.png'
                     alt=""
-                />
+                /> */}
 
-        <h1 className={cx('title')}>PHIM ĐANG CHIẾU</h1>
+        {/* <h1 className={cx('title')}>PHIM ĐANG CHIẾU</h1> */}
         </div>
     );
     
