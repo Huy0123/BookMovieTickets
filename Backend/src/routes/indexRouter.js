fca7483d8972efe00b7609e7ae3248e305c97afd
Huy0123 <voquochuysn2004@gmail.com>
2024-10-12 11:18:27 +0700

    add

--- Backend/src/routes/indexRouter.js
+++ Backend/src/routes/indexRouter.js
@@ -8,7 +8,7 @@ const seat = require('../routes/SeatRouter.js')
 const cinema = require('../routes/CinemaRouter.js')
 const promotion = require('../routes/PromotionRouter.js')
 const room = require('../routes/RoomRouter.js')
-
+const seatTime = require('../routes/SeatTimeRouter.js')
 function route(app){
     app.use('/v1/Users',Users)
     app.use('/v1/Payment',Payment)
@@ -17,13 +17,12 @@ function route(app){
         res.send('API đang hoạt động!');
     });
     app.use('/v1',movie)
-    // app.use('/v1',booking)
     app.use('/v1', showtime)
     app.use('/v1',seat)
     app.use('/v1', cinema)
-    // app.use('/v1',payment)
     app.use('/v1',promotion)
     app.use('/v1',room)
+    app.use('/v1',seatTime)
 
 }
 module.exports = route;
