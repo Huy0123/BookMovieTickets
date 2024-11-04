3eacd4fc9f0cdcfdf71d8ec5e2b66e245b9c78a6
chodu <shenlol184@gmail.com>
2024-11-04 14:29:03 +0700

    pull

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -14,6 +14,15 @@ import BookTicket from '~/pages/BookTicket';
 import Payment from '~/pages/Payment';
 import ProfileLayout from '~/Components/Layout/ProfileLayout';
 import Thanks from '~/pages/Thanks';
+import AdminLayout from '~/Components/Layout/AdminLayout';
+import AddMovie from '~/Admin/AddMovie';
+import EditCinema from '~/Admin/EditCinema';
+import EditMovie from '~/Admin/EditMovie';
+import MemberList from '~/Admin/MemberList';
+import CinemaList from '~/Admin/CinemaList';
+import MovieList from '~/Admin/MovieList';
+
+import Moderator from '~/pages/Moderator';
 // Không cần đăng nhập vẫn xem được
 const publicRoutes = [
     { path: '/', component:Home },
@@ -29,6 +38,15 @@ const publicRoutes = [
     { path: '/forgot', component:ForgotPassword, layout:null},
     { path: '/reset-password/:token', component: ResetPassword, layout: null }, 
     { path: '/payment', component:Payment},
+    { path: '/admin/addMovie', componment:AddMovie , layout: AdminLayout }, 
+    { path: '/admin/editCinema', component:EditCinema , layout: AdminLayout }, 
+    { path: '/admin/editMovie', component:EditMovie , layout: AdminLayout },
+    { path: '/admin/cinemalist', component:CinemaList , layout: AdminLayout }, 
+    { path: '/admin/movielist', component:MovieList , layout: AdminLayout }, 
+    { path: '/admin/memberlist', component:MemberList , layout: AdminLayout }, 
+   
+
+    { path: '/moderator/*', component:Moderator},
     // { path: '/CinemaManeger', component:CinemaManeger},
 ]
 //Ngược lại
